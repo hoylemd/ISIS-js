7215cfc5b466c430541c830f64c53532f3280461
Michael D. Hoyle <hoylemd@gmail.com>
2012-08-22 23:18:07 -0400

    Refactored to use top-level ISIS object

--- ISISgame.js
+++ ISISgame.js
@@ -1,80 +1,86 @@
-window.onload = function() {
-		// declare global variables
-		var objCanvas;
-		var objContext;
-		var imgFloorTile;
-		var imgWallTile;
-		var imgCornerTile;
-		var imgRedStar;
-		var imgBlueStar;
-	
-		/* Function to load up all neccesary content */
-		var loadContent = function(){
-			imgSpaceTile = loadImage("space.png");
-			imgFloorTile = loadImage("floor1.png");
-			imgWallTile = loadImage("wall1.png");
-			imgCornerTile = loadImage("corner1.png");
-			imgRedStar = loadImage("redstar.png");
-			imgBlueStar = loadImage("bluestar.png");
-			imgImmortal1 = loadImage("Immortal1.png");
-			imgImmortal2 = loadImage("Immortal2.png");
-		}();
-	
-		// function to reset a context's transformation matrix
-		function resetContext(objContext)
-		{
-			objContext.setTransform(1, 0, 0, 1, 0, 0);	
-		}
-	
-		/* Function to draw the background tiles
-		 * objContext: the context object to draw the tiles to. */
-		function drawBackground(objContext)
-		{
+window.onload = function() 
+{
+	// create top-level object
+	var ISIS = function()
+	{
+		/* graphics objects */
+		var objCanvas = document.getElementById("myCanvas");
+		var objContext = objCanvas.getContext("2d");
+		
+		/* Load up all neccesary content */
+		var objImageLibrary = {
+			spaceTile : loadImage("space.png"),
+			immortal1 : loadImage("Immortal1.png"),
+			immortal2 : loadImage("Immortal2.png")
+		};
 
-			// Prepare for next round of drawing			
-			resetContext(objContext);
-			objContext.clearRect(0, 0, objCanvas.width, objCanvas.height);
-			
+		/* Function to reset the canvas context */
+		var funResetContext = function()
+		{	
+			objContext.setTransform(1, 0, 0, 1, 0, 0);	
+		};
+
+		/* Function to redraw the background */
+		var funDrawBackground = function ()
+		{
+			// set the context to the tile offset
 			objContext.translate(50, 50)
-			// draw the middle rows
+		
+			// draw rows
 			for (var i = 0; i < 15; i++)
 			{
-				// draw the top row
+				// draw each tile
 				for(var j = 0; j < 15; j++)
 				{
-					objContext.drawImage(imgSpaceTile, -50, -50);
+					objContext.drawImage(objImageLibrary["spaceTile"], -50, -50);
 					objContext.translate(100, 0);
 				}
 			
 				objContext.translate(-1500, 100);
 			}
 
-			objContext.translate(-1500, -1500);
-		}	
+			// reset the context
+			funResetContext(objContext);
+		};
 
-		// get the canvas and context
-		objCanvas = document.getElementById("myCanvas");
-		objContext = objCanvas.getContext("2d");
-		
+		var funUpdate = function()
+		{
+			// Prepare for next round of drawing			
+			objContext.clearRect(0, 0, 1500, 1500);
+			cfnResetContext();
+
+			funDrawBackground();
+		}
 
 		//Add an event listener for mouse clicks
-		objCanvas.addEventListener('click', function(evt){
+		objCanvas.addEventListener('click', 
+			function(evt)
+			{
+				// get the mouse position
+				var mousePos = getMousePos(objCanvas, evt);
+				
+				// redraw the background
+				funDrawBackground(objContext);
+				
+				funResetContext(objContext);
+				
+				objContext.translate(mousePos.x, mousePos.y);
+			
+				// draw the image
+				objContext.drawImage(objImageLibrary["immortal1"], -40, -40);
 
-			// get the mouse position
-			var mousePos = getMousePos(objCanvas, evt);
+				funResetContext(objContext);
 			
-			// redraw the background
-			drawBackground(objContext);
-			
-			resetContext(objContext);
-			
-			objContext.translate(mousePos.x, mousePos.y);
-			
-			// draw the image
-			objContext.drawImage(imgImmortal1, -40, -40);
-		})
-	
+			}
+		);	
 
-		drawBackground(objContext);
-		drawBackground(objContext);
-	};
+		return {
+			context : objContext,
+			images : objImageLibrary,
+			resetContext : funResetContext,
+			drawBackground : funDrawBackground
+		}	
+	}();
+
+	ISIS.drawBackground();
+};
