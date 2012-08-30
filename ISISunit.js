2644ea9cb00e1b98b4db84ab5903567b84b0fb2b
Michael D. Hoyle <hoylemd@gmail.com>
2012-08-29 23:13:34 -0400

    Added correct movement angle calculation

--- ISISunit.js
+++ ISISunit.js
@@ -1,8 +1,10 @@
+Math.TAU = 2 * Math.PI;
 
 var ISIS_unit = function(context)
 {
 	var tileSize = 100;
 	var offset = 0;
+	var rotation = 0;
 
 	// Unit class prototype function (hidden)
 	var unit_prototype =
@@ -13,7 +15,6 @@ var ISIS_unit = function(context)
 
 		// movement function
 		moveTo : function(intX, intY){
-
 			//alert(intX + ", " + intY + ", " + tileSize); 			
 			var xOffset = intX % tileSize;
 			var yOffset = intY % tileSize;
@@ -22,8 +23,51 @@ var ISIS_unit = function(context)
 
 			//alert((intX - xOffset) + ", " + (intY - yOffset));
 
-			this.x = intX - xOffset;
-			this.y = intY - yOffset;
+			var newX = intX - xOffset;
+			var newY = intY - yOffset;
+			
+			// calculate rotation
+			var dx = newX - this.x;
+			var dy = newY - this.y;
+
+			alert(dx + ", " + dy);
+			// determine quadrant
+			if (dy < 0)
+			{
+				if (dx == 0)
+				{
+					rotation = 0;
+				}
+				else
+				{
+					rotation = -1 * Math.atan(dx / dy);
+				}
+			}
+			else if (dy == 0)
+			{
+				if (dx > 0)
+				{
+					rotation = Math.TAU * 0.25;
+				}
+				else if (dx < 0)
+				{
+					rotation = Math.TAU * -0.25;
+				}
+			}
+			else
+			{
+				if (dx == 0)
+				{
+					rotation = Math.TAU * 0.5;
+				}
+				else
+				{
+					rotation =  Math.TAU * 0.5 - Math.atan(dx / dy) ;
+				}
+			}
+
+			this.x = newX;
+			this.y = newY;
 		},
 
 		// drawing function
@@ -35,8 +79,12 @@ var ISIS_unit = function(context)
 			// draw the sprite image if it exists
 			if (this.image)
 			{
-				context.translate(this.x + offset, this.y + offset);
-				context.drawImage(this.image, 0, 0);
+				context.translate(this.x + 0.5 * tileSize,
+					   	this.y + 0.5 * tileSize);
+				context.rotate(rotation);
+				context.translate();
+				context.drawImage(this.image, -1 * (0.5 * tileSize - offset), 
+					-1 * (0.5 * tileSize - offset));
 				context.reset();
 			}
 		}
