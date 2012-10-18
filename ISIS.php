607f885617528b9fe8f4a8ddf9ac0923383436b4
Michael D. Hoyle <hoylemd@gmail.com>
2012-10-17 22:16:38 -0400

    Added sprite file and removed ISIS prefix on js files

--- ISIS.php
+++ ISIS.php
@@ -8,10 +8,11 @@
 
 		Click the "Move" button to choose where to move your ship, then click "Go" to move it there. Click the "Attack" button to choose an attack target.
 
-		<script type = "text/javascript" src = "ISISUtils.js"></script>
-		<script type = "text/javascript" src = "ISISgame.js"></script>
-		<script type = "text/javascript" src = "ISISunit.js"></script>
-		<script type = "text/javascript" src = "ISISorder.js"></script>
+		<script type = "text/javascript" src = "Utils.js"></script>
+		<script type = "text/javascript" src = "sprite.js"></script>
+		<script type = "text/javascript" src = "game.js"></script>
+		<script type = "text/javascript" src = "unit.js"></script>
+		<script type = "text/javascript" src = "order.js"></script>
 		<script type = "text/javascript" src = "main.js"></script>
 	</body>
 </html>
