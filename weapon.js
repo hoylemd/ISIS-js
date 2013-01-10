a082294b3c5640239acd7896f9fd232aab5f626a
hoylemd <hoylemd@gmail.com>
2013-01-09 20:05:20 -0500

    #18: Added fire method

--- weapon.js
+++ weapon.js
@@ -3,16 +3,46 @@
 var ISIS_weapon = function () {
 	// prototype
 	var weapon_prototype = {
+		fire : function (target) {
+			var roll;
+			var dodge;
+
+			// do attack roll
+			roll = Math.d100();
+			dodge = target.dodge();
+
+			// damage, if applicable
+			if (roll + this.hit_bonus > 50 + dodge)
+			{
+				console.log(this.name + " hits(" + roll + "/"
+					+ (50 + dodge) + ") " + target.name + " for "
+					+ this.damage +" points of damage");
+				target.takeDamage(this.damage);
+			}
+			else
+			{
+				console.log(this.name + " misses(" + roll + "/" +
+					(50 + dodge) + ") " + target.name);
+			}
+
+			// play animation
+			if (this.proj_texture != null) {
+
+			}
+		}
+
 	};
 
 	// constructor
-	return function (damage, hit_bonus, recycle, texture, speed)
+	return function (name, damage, hit_bonus, recycle, texture, speed)
 	{
 		var new_weapon = {
 			__proto__ : weapon_prototype
 		};
 
 		if (texture) {
+			new_weapon.name = name;
+
 			new_weapon.damage = damage;
 			new_weapon.hit_bonus = hit_bonus;
 			new_weapon.recycle = recycle;
