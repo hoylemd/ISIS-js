475c042d202d55008f4c9db4fe38acc2901e2f13
hoylemd <hoylemd@gmail.com>
2013-01-09 20:29:04 -0500

    #18: Added weaposn to test ships

--- game.js
+++ game.js
@@ -80,9 +80,13 @@ var ISIS_engine = function()
 	var funInitGame = function(){
 		player = unit("ArkadianCruiser", {x:1, y:1}, 0);
 		player.name = "Arkadian Cruiser";
+		player.setHull(100);
+		player.addWeapon(Weapon("Arkadian Railgun", 10, 25, 15, images["bullet"], 25));
 
 		enemy = unit("TerranCruiser", {x:1, y:1}, 0);
 		enemy.name = "Terran Cruiser";
+		enemy.setHull(150);
+		enemy.addWeapon(Weapon("Terran Mass Driver", 25, 10, 30, images["bullet"], 20));
 
 		playerFleetView = fleetView(images["spaceTile"], SpriteManager());
 		playerFleetView.move(0, 0);
