e58186b29e84a2927af4f4cea1a868a860ef951d
hoylemd <hoylemd@gmail.com>
2013-01-18 00:22:21 -0500

    #2: refactores to use better data structures and to bring in line with newer code style

--- spriteManager.js
+++ spriteManager.js
@@ -1,62 +1,51 @@
 // Sprite manager object
 
-function ISIS_spriteManager(Sprite, canvas)
+function ISIS_spriteManager (Sprite, canvas)
 {
 	var spriteManager_prototype =
 	{
-		spriteList : null,
-		numSprites : 0,
-		msBetweenFrames : 0,
-		msSinceLastFrame : 0,
-
-		addSprite : function(sprite, id) {
+		addSprite : function (sprite) {
 			var newId = this.numSprites;
+
 			if (sprite && this.spriteList) {
-				if (id)
-					this.spriteList[id] = sprite;
 				this.spriteList[newId] = sprite;
 				this.numSprites += 1;
 				sprite.register(this);
 				return sprite;
-			}
-			else {
+			} else {
 				return null;
 			}
 		},
 
-		newSprite : function(image, mapDims, msBetweenFrames, id) {
+		newSprite : function (image, mapDims, msBetweenFrames) {
 			var new_sprite = Sprite(image, mapDims, msBetweenFrames);
 			return this.addSprite(new_sprite);
 		},
 
-		removeSprite : function(sprite)
-		{
+		removeSprite : function (sprite) {
 			var index = "";
 
-			if (sprite && this.spriteList)
-			{
-				for (index in this.spriteList)
-				{
-					if (this.spriteList[index] === sprite){
-						delete this.spriteList[index];
+			if (sprite && this.spriteList) {
+				for (index in this.spriteList) {
+					if (this.spriteList[index] === sprite) {
+						this.spriteList.splice(index, 1);
 						this.numSprites -= 1;
 					}
 				}
 			}
 		},
 
-
-		update : function(elapsed)
-		{
+		update : function (elapsed) {
 			var index = "";
 			var sprite = null;
+
 			for (index in this.spriteList){
 				sprite = this.spriteList[index];
 				if (sprite.position.x > this.canvas.width ||
 					sprite.position.x < 0 ||
 					sprite.position.y > this.canvas.height ||
 					sprite.position.y < 0) {
-					sprite.destruct();
+					sprite.dispose();
 					sprite = null;
 				}
 
@@ -76,14 +65,15 @@ function ISIS_spriteManager(Sprite, canvas)
 		}
 	}
 
-	return function ( canvas)
-	{
+	return function (canvas) {
 		var new_spriteManager = {
 			__proto__: spriteManager_prototype
 		};
 
+		new_spriteManager.spriteList = null;
+		new_spriteManager.numSprites = 0;
 		new_spriteManager.canvas = canvas;
-		new_spriteManager.spriteList = {};
+		new_spriteManager.spriteList = [];
 
 		return new_spriteManager;
 	}
