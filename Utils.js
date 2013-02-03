39656569f275fbea39069f9169f6108da7be52ca
hoylemd <hoylemd@gmail.com>
2013-02-03 10:24:47 -0500

    fixed requestAnimationFrame code so it's not ugly anymore

--- Utils.js
+++ Utils.js
@@ -130,33 +130,39 @@ Math.calculateLineAngle = function(p1, p2)
 	return rotation;
 };
 
-// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+// Adapted from http://my.opera.com/emoller/blog/2011/12/20/reauestanimationframe-for-smart-er-animating
+// incorporating fixes from Erik Moller, Paul Irish and Tino Zijdel
 
-// requestAnimationFrame polyfill by Erik Möller
-// fixes from Paul Irish and Tino Zijdel
+( function () {
+	var lastTime = 0;
+	var vendors = ['ms', 'moz', 'webkit', 'o'];
+	var rafStr = 'RequestAnimationFrame';
+	var cafStr = 'CancelAnimationFrame';
+	var crafStr = 'CancelRequestAnimationFrame';
 
-(function() {
-    var lastTime = 0;
-    var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
-                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
-    }
+	for (var x = 0; x < vendors.length && !window.requestAnimationFrame;
+		x++) {
+		var prefix = vendors[x];
+		window.requestAnimationFrame = window[prefix + rafStr];
+		window.cancelAnimationFrame = window[prefix + cafStr] ||
+			window[prefix + crafStr];
+	}
 
-    if (!window.requestAnimationFrame)
-        window.requestAnimationFrame = function(callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
-              timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
+	if (!window.requestAnimationFrame) {
+		window.requestAnimationFrame = function (callback, element) {
+			var currTime = new Date().getTime();
+			var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+			var id = window.setTimeout(function () {
+				callback(currTime + timeToCall);
+			}, timeToCall);
+			lastTime = currTime + timeToCall;
+			return id;
+		};
+	}
 
-    if (!window.cancelAnimationFrame)
-        window.cancelAnimationFrame = function(id) {
-            clearTimeout(id);
-        };
-}());
+	if (!window.cancelAnimationFrame) {
+		window.cancelAnimationFrame = function (id) {
+			clearTimeout(id);
+		};
+	}
+}() );
