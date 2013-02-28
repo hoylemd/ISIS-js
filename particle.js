23f8b18c88687475fa2f2bb59dfa10743fb6802e
hoylemd <hoylemd@gmail.com>
2013-02-27 22:36:55 -0500

    minor refactors

--- particle.js
+++ particle.js
@@ -2,44 +2,39 @@
 
 function ISIS_ParticleManager () {
 	var manager_proto = new ISIS_manager();
+
+	// particle object prototype
 	var particle_prototype = {
 		__proto__ : manager_proto.type_proto,
-		checkContinue : function (displacement) {
-			return this.remaining > 0;
-		},
-
 		update : function (elapsed) {
+			// skip update if there's been no time between updates
 			if (!elapsed) {
 				return;
 			}
-			if (this.sprite === null || this.done) {
-				this.dispose();
-				return;
-			}
 
+			// calculate the movement for this update
 			this.remaining = this.remaining - elapsed;
 			var speed = elapsed / this.time;
 			var displacement = {x: this.vector.x * speed,
 				y: this.vector.y * speed};
 
+			// apply the movement animation
+			this.sprite.rotate(this.rotation);
+			this.sprite.move(displacement);
 
-			this.done = !this.checkContinue(displacement);
-
-			if (this.rotation) {
-				this.sprite.rotate(this.rotation);
-			}
-
+			// fade, if set
 			if (this.fade) {
 				this.sprite.setAlpha(this.remaining / this.time);
 			}
 
-			this.sprite.move(displacement);
+			// check for continuation
+			if (this.remaining <= 0) {
+				this.dispose();
+			}
+
 		},
 
-		register : function (manager) {
-			this.manager = manager;
-		},
-
+		// destructor
 		dispose : function () {
 			if (this.manager) {
 				this.manager.remove(this);
@@ -50,16 +45,17 @@ function ISIS_ParticleManager () {
 		}
 	};
 
+	// manager prototype
 	var particleManager_prototype = {
 		__proto__ : manager_proto,
-		create :
-			function (sprite, origin, destination, time, rotation, fade) {
-
+		create : function (sprite, origin, destination, time, rotation, fade) {
+			// prototype the object
 			var new_particle = {
 				__proto__ : particle_prototype,
 				manager : this
 			}
 
+			// set the fields
 			if (sprite && origin && destination && time) {
 				new_particle.sprite = sprite;
 				new_particle.destination = destination;
@@ -75,6 +71,7 @@ function ISIS_ParticleManager () {
 				new_particle.vector = {x: destination.x - origin.x,
 					y: destination.y - origin.y};
 
+			// handle errors
 			} else {
 				console.log("particle is missing some arguments");
 				new_particle = null;
@@ -83,34 +80,34 @@ function ISIS_ParticleManager () {
 			return this.add(new_particle);
 		},
 
+		// constructor for debris type particles from an existing sprite
 		newDebris : function(sprite) {
 			var position = sprite.center();
-			// choose rotation & rate
+
+			// choose rotation rate
 			var rate = (Math.random() - 0.5) / 5;
+
 			// choose destination
 			var destination = {x: position.x + (Math.random() * 300) - 150,
 				y: position.y + (Math.random() * 300) - 150};
+
+			// cascade into the particle constructor
 			this.create(sprite, position, destination, 1000, rate, true);
 		},
 
+		// update method
 		update : function (elapsed) {
 			var index = "";
 			var particle = null;
 			for (index in this.object_list){
 				particle = this.object_list[index];
 
-				if (particle) {
-					particle.update(elapsed);
-				}
+				particle.update(elapsed);
 			}
 		}
 	}
 
 	return function () {
-		var new_particleManager = {
-			__proto__: particleManager_prototype
-		};
-
-		return new_particleManager;
+		this.__proto__ = particleManager_prototype;
 	}
 }
