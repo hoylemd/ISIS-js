5edd53cabbc40f6df5c834bf3bbb814e6ddded7c
hoylemd <hoylemd@gmail.com>
2013-03-02 22:41:57 -0500

    Refactored to new sprite manager constructor

--- battle_state.js
+++ battle_state.js
@@ -17,7 +17,7 @@ var ISIS_battleState = function (game, canvas, content) {
 	var playerFleetView;
 
 	// sprite objects
-	var spriteManager = new ISIS_sprite_manager(canvas);
+	var spriteManager = new ISIS.SpriteManager(canvas);
 
 	// Particle objects
 	var particle_manager = new (ISIS_ParticleManager())();
