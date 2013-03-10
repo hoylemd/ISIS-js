6eccb24a5ea6d792ae33171f6e6bb88eb9efbe9e
hoylemd <hoylemd@gmail.com>
2013-03-09 23:58:56 -0500

    moved class library instantiation into the Engine

--- main.js
+++ main.js
@@ -7,14 +7,6 @@ window.onload = function () {
 	var canvas = document.getElementById("myCanvas");
 	var io = new ISIS_IO();
 
-	// initialize class library
-	ISIS.Manager = ISIS_Manager();
-	ISIS.SpriteManager = ISIS_SpriteManager(canvas);
-	ISIS.ParticleManager = ISIS_ParticleManager();
-	ISIS.ProjectileManager = ISIS_ProjectileManager();
-	ISIS.FleetView = ISIS_fleetView(canvas);
-	ISIS.GameState = ISIS_gameState();
-
 	// set up the main engine
 	ISIS.Engine = ISIS_Engine(canvas, io);
 
