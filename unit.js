4b32efbede1ec4e2891badb5b94514d50b55e922
hoylemd <hoylemd@gmail.com>
2013-03-24 22:19:18 -0400

    cleaned up code

--- unit.js
+++ unit.js
@@ -1,6 +1,15 @@
-// Unit object (unmanaged)
-var ISIS_unit = function(context, content, spriteManager, particle_manager)
-{
+// Unit object
+var ISIS_UnitManager = function (canvas, content) {
+	// get the context
+	var context = canvas.getContext("2d");
+
+	// check the params
+	if (!context) {
+		throw "no context given to Pokemon class constructor";
+	} else if (!content) {
+		throw "no content given to Pokemon class constructor";
+	}
+
 	// tile constant
 	var tileSize = 100;
 
@@ -20,7 +29,7 @@ var ISIS_unit = function(context, content, spriteManager, particle_manager)
 	// function for when the unit is destroyed
 	var wreck =  function (that) {
 		// turn the ship sprite into debris
-		new particle_manager.Debris(that.sprite);
+		new that.manager.particle_manager.Debris(that.sprite);
 
 		console.log("Unit " + that.name + " destroyed!");
 
@@ -45,6 +54,12 @@ var ISIS_unit = function(context, content, spriteManager, particle_manager)
 			this.sprite.centerOn(this.position);
 			bar_y = (this.position.y + 0.5 * this.sprite.frameDims.y) - 15;
 			this.health_bar.centerOn({x: this.position.x, y: bar_y });
+		},
+
+		move : function (displacement) {
+			var newPos = {x: this.position.x + displacement.x,
+				y: this.position.y + displacement.y};
+			this.moveTo(newPos);
 		},
 
 		// arbtrary rotation function
@@ -87,7 +102,7 @@ var ISIS_unit = function(context, content, spriteManager, particle_manager)
 				this.weapon = weapon;
 				weapon.install(this);
 			} else {
-				console.log("Cannot add a null weapon");
+				throw "Cannot add a null weapon";
 			}
 		},
 
@@ -99,12 +114,10 @@ var ISIS_unit = function(context, content, spriteManager, particle_manager)
 
 		// order registration
 		registerOrder : function (order) {
-			if (order.name === "attack") {
-				order.x = order.target.x;
-				order.y = order.target.y;
-				order.owner = this;
-				this.orders.attack = order;
-			}
+			order.x = order.target.x;
+			order.y = order.target.y;
+			order.owner = this;
+			this.orders[order.name] = order;
 		},
 
 		// carry out orders function
@@ -112,19 +125,21 @@ var ISIS_unit = function(context, content, spriteManager, particle_manager)
 			var attack = this.orders.attack;
 
 			// attack order
-			if (attack) {
-				if (attack.pending) {
-					if (this.weapon) {
-						this.weapon.setTarget(attack.target);
-						attack.pending = false;
-					} else {
-						console.log(this.name +
-							" has no weapon! It cannot attack!");
-						this.orders.attack = null;
+			if (this.weapon) {
+				if (attack) {
+					if (attack.pending) {
+						if (this.weapon) {
+							this.weapon.setTarget(attack.target);
+							attack.pending = false;
+						} else {
+							console.log(this.name +
+								" has no weapon! It cannot attack!");
+							this.orders.attack = null;
+						}
 					}
+				} else {
+					this.weapon.setTarget(null);
 				}
-			} else {
-				this.weapon.setTarget(null);
 			}
 		},
 
@@ -136,14 +151,15 @@ var ISIS_unit = function(context, content, spriteManager, particle_manager)
 
 			// spawn debris
 			texture = this.debris[Math.floor(Math.random() * 3)];
-			sprite = new spriteManager.Sprite(texture, {x: 1, y: 1}, 0);
+			sprite = new this.manager.sprite_manager.Sprite(
+				texture, {x: 1, y: 1}, 0);
 
 			// positioning
 			sprite.setRotation = Math.random();
 			sprite.centerOn(position);
 
 			// spawn debris
-			new particle_manager.Debris(sprite);
+			new this.manager.particle_manager.Debris(sprite);
 
 			// check if the unit is wrecked
 			if (this.hullCurrent <= 0) {
@@ -176,8 +192,15 @@ var ISIS_unit = function(context, content, spriteManager, particle_manager)
 		// update function
 		update : function (elapsed_ms) {
 			// update children
-			this.weapon.update(elapsed_ms);
+			if (this.weapon) {
+				this.weapon.update(elapsed_ms);
+			}
 			this.health_bar.value = this.hullCurrent / this.hullMax;
+
+			// run AI if it exists
+			if (this.AI) {
+				this.AI();
+			}
 
 			// check if attack order is still valid
 			if (this.orders.attack) {
@@ -203,39 +226,73 @@ var ISIS_unit = function(context, content, spriteManager, particle_manager)
 			delete this.health_bar;
 			this.sprite.dispose();
 			delete this.sprite;
+			this.manager.remove(this);
 		}
 	}
 
-	// constructor
-	return function(texture, mapDims, msBetweenFrames, debris) {
-		// prepare sprites
-		var sprite = new spriteManager.Sprite(
-			(content[texture]), mapDims, msBetweenFrames);
-		var health_bar_dims = {x: sprite.frameDims.x * 0.8, y: 10};
-		var health_bar = new spriteManager.BarSprite(health_bar_dims,
-			"green", "red", "yellow", 0.2);
+	// Unit constructor constructor
+		unitConstructor = function (manager) {
+			return function(texture, mapDims, msBetweenFrames, debris) {
 
-		// build the object
-		this.__proto__ = unit_prototype;
+			// prepare sprites
+			var sprite = new manager.sprite_manager.Sprite(
+				content.images[texture], mapDims, msBetweenFrames);
+			var health_bar_dims = {x: sprite.frameDims.x * 0.8, y: 10};
+			var health_bar = new manager.sprite_manager.BarSprite(health_bar_dims,
+				"green", "red", "yellow", 0.2);
 
-		// sprites
-		this.debris= debris;
-		this.sprite= sprite;
-		this.health_bar = health_bar;
+			// build the object
+			this.__proto__ = unit_prototype;
 
-		// general stuff
-		this.name= "Unnamed Unit";
-		this.position = sprite.position;
-		this.rotation = sprite.rotation;
-		this.fleetView = null;
+			// link to the manager
+			this.manager = manager;
 
-		// combat stats
-		this.orders = {};
-		this.hullMax = 5;
-		this.hullCurrent = 5;
-		this.dodgeBonus = 10;
-		this.weapon = null;
-		this.destroyed = false
+			// sprites
+			this.debris= debris;
+			this.sprite= sprite;
+			this.health_bar = health_bar;
 
-	}
+			// general stuff
+			this.name= "Unnamed Unit";
+			this.position = sprite.position;
+			this.rotation = sprite.rotation;
+			this.fleetView = null;
+
+			// combat stats
+			this.orders = {};
+			this.hullMax = 5;
+			this.hullCurrent = 5;
+			this.dodgeBonus = 10;
+			this.weapon = null;
+			this.destroyed = false
+
+			// add to the manager
+			manager.add(this);
+
+		};
+	};
+
+	var checkCollisions = function (point) {
+		var colliders = [];
+		for (var i in this.object_list) {
+			var current = this.object_list[i];
+			if (current.collide && current.collide(point)) {
+				colliders.push(current);
+			}
+		}
+
+		return colliders;
+	};
+
+	return function (spriteManager, particle_manager){
+		this.__proto__ = new ISIS.Manager();
+		this.type_proto = unit_prototype;
+		this.sprite_manager = spriteManager;
+		this.particle_manager = particle_manager;
+
+		this.checkForCollisions = checkCollisions;
+		this.Unit = unitConstructor(this);
+
+		this.canvas = canvas;
+	};
 }
