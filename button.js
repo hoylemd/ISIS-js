3284763534d2d3d3eeaba3cbf2facf0988d47b71
hoylemd <hoylemd@gmail.com>
2013-06-01 18:27:29 -0400

    #50: Implemented button class

--- button.js
+++ button.js
@@ -1,5 +1,10 @@
-// button code file
-var ISIS_Button = function () {
+var ISIS_Button = function (sprite_manager, clickable_manager) {
+	var buttonAnimation = function (handler) {
+		return function (params) {
+			console.log("button pressed");
+			handler(params);
+		};
+	};
 
 	var button_prototype = {
 
@@ -7,8 +12,25 @@ var ISIS_Button = function () {
 
 	var Button = function (params) {
 		this.__proto__ = button_prototype;
-	};
+		if (params['dimensions'] &&
+			params['active_colour'] &&
+			params['inactive_colour'] &&
+			params['text'] &&
+			params['font'] &&
+			params['font_active_colour'] &&
+			params['font_inactive_colour'] &&
+			params['handler']){
+			this.sprite = new sprite_manager.ButtonSprite(params);
 
+			// wrap the handler
+			params['handler'] = buttonAnimation(params['handler']);
+
+			this.clickable = new clickable_manager.Clickable(params);
+
+		} else {
+			throw new "Invalid Button Parameters";
+		}
+	}
 	return Button;
 
 };
