5e16b9cb03a343440d1935fda133b721a5b4b639
Michael Hoyle <mike@filemobile.com>
2013-06-01 22:24:40 -0400

    #50: added manager

--- button.js
+++ button.js
@@ -1,4 +1,5 @@
-var ISIS_Button = function (sprite_manager, clickable_manager) {
+// code file for button managers
+var ISIS_ButtonManager = function () {
 	var buttonAnimation = function (handler) {
 		return function (params) {
 			console.log("button pressed");
@@ -20,17 +21,29 @@ var ISIS_Button = function (sprite_manager, clickable_manager) {
 			params['font_active_colour'] &&
 			params['font_inactive_colour'] &&
 			params['handler']){
-			this.sprite = new sprite_manager.ButtonSprite(params);
+			this.sprite = new manager.sprite_manager.ButtonSprite(params);
 
 			// wrap the handler
 			params['handler'] = buttonAnimation(params['handler']);
 
-			this.clickable = new clickable_manager.Clickable(params);
+			this.clickable = new manager.clickable_manager.Clickable(params);
 
 		} else {
 			throw new "Invalid Button Parameters";
 		}
+	};
+
+	var ButtonManager = function (params) {
+		this.__proto__ = new ISIS.Manager();
+
+		// connect to relevant type managers
+		this.sprite_manager = params["sprite_manager"];
+		this.clickable_manager = params["clickable_manager"];
+
+		// install the Button Constructor
+		this.Button = Button;
 	}
-	return Button;
+
+	return ButtonManager;
 
 };
