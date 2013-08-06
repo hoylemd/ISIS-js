9aba139209cd6fb2477c09e579a90e82f9a99bf3
Michael Hoyle <mike@filemobile.com>
2013-08-06 13:11:22 -0400

    #55: Updated the vector test specs to use check that the constructors are actually making Vectors

--- spec/specVector.js
+++ spec/specVector.js
@@ -1,11 +1,23 @@
 // testing suite for vectors
 describe("Vectors", function () {
+	// grab the vector class
 	var Vector = ISIS_Vector();
 	var primitive = {x: 1.5, y: -2};
 
+	// add the vector class matcher
+	beforeEach(function() {
+	  this.addMatchers({
+		toBeVectorClass: function(expectedSong) {
+		  var obj = this.actual;
+		  return Vector.isPrimitiveVector(obj._internals);
+		}
+	  });
+	});
+
+	// test constructors
 	describe("can be built by", function () {
 		it("a primitive vector", function () {
-			expect(new Vector(primitive)).toBeTruthy();
+			expect(new Vector(primitive)).toBeVectorClass();
 		});
 	});
 });
